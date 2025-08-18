9e21b1113c062ff9611c591f1022fb02e89dbb08
liuweile <1209373349@qq.com>
2025-08-18 14:01:24 +0800

    admob 适配器优化

--- AlxAdsDemo/AdmobAdapter/AlgorixAdmobRewardVideoAdapter.swift
+++ AlxAdsDemo/AdmobAdapter/AlgorixAdmobRewardVideoAdapter.swift
@@ -9,20 +9,20 @@ import Foundation
 import GoogleMobileAds
 import AlxAds
 
-@objc(AlgorixAdmobRewardAdapter)
-public class AlgorixAdmobRewardAdapter:AlgorixAdmobBaseAdapter,MediationRewardedAd {
+@objc(AlgorixAdmobRewardVideoAdapter)
+public class AlgorixAdmobRewardVideoAdapter:AlgorixAdmobBaseAdapter,MediationRewardedAd {
     
-    private static let TAG = "AlgorixAdmobRewardAdapter"
+    private static let TAG = "AlgorixAdmobRewardVideoAdapter"
     
     private var rewardedAd: AlxRewardVideoAd? = nil
     private var delegate: MediationRewardedAdEventDelegate? = nil
     private var completionHandler: GADMediationRewardedLoadCompletionHandler? = nil
    
     public func loadRewardedAd(for adConfiguration: MediationRewardedAdConfiguration, completionHandler: @escaping GADMediationRewardedLoadCompletionHandler) {
-        NSLog("%@: loadRewardedAd",AlgorixAdmobRewardAdapter.TAG)
+        NSLog("%@: loadRewardedAd",AlgorixAdmobRewardVideoAdapter.TAG)
         guard let params = AlgorixAdmobBaseAdapter.parseAdparameter(for: adConfiguration.credentials) else {
             let errorStr="The parameter field is not found in the adConfiguration object"
-            NSLog("%@: config params is empty",AlgorixAdmobRewardAdapter.TAG)
+            NSLog("%@: config params is empty",AlgorixAdmobRewardVideoAdapter.TAG)
             self.delegate=completionHandler(nil,self.error(code: -100,msg: errorStr))
             return
         }
@@ -33,12 +33,12 @@ public class AlgorixAdmobRewardAdapter:AlgorixAdmobBaseAdapter,MediationRewarded
         
         guard let adId = params["unitid"] as? String,!adId.isEmpty else{
             let errorStr="unitid is empty in the parameter configuration"
-            NSLog("%@: error: %@",AlgorixAdmobRewardAdapter.TAG,errorStr)
+            NSLog("%@: error: %@",AlgorixAdmobRewardVideoAdapter.TAG,errorStr)
             self.delegate=completionHandler(nil,self.error(code: -100,msg: errorStr))
             return
         }
         
-        NSLog("%@: loadRewardedAd unitid=%@",AlgorixAdmobRewardAdapter.TAG,adId)
+        NSLog("%@: loadRewardedAd unitid=%@",AlgorixAdmobRewardVideoAdapter.TAG,adId)
         self.completionHandler = completionHandler
         
         // load ad
@@ -48,7 +48,7 @@ public class AlgorixAdmobRewardAdapter:AlgorixAdmobBaseAdapter,MediationRewarded
     }
     
     public func present(from viewController: UIViewController) {
-        NSLog("%@: present",AlgorixAdmobRewardAdapter.TAG)
+        NSLog("%@: present",AlgorixAdmobRewardVideoAdapter.TAG)
         if let rewardedAd = self.rewardedAd,rewardedAd.isReady(){
             rewardedAd.showAd(present: viewController)
         }
@@ -56,55 +56,55 @@ public class AlgorixAdmobRewardAdapter:AlgorixAdmobBaseAdapter,MediationRewarded
     
 }
 
-extension AlgorixAdmobRewardAdapter:AlxRewardVideoAdDelegate{
+extension AlgorixAdmobRewardVideoAdapter:AlxRewardVideoAdDelegate{
     
     public func rewardVideoAdLoad(_ ad: AlxRewardVideoAd) {
-        NSLog("%@: rewardVideoAdLoad",AlgorixAdmobRewardAdapter.TAG)
+        NSLog("%@: rewardVideoAdLoad",AlgorixAdmobRewardVideoAdapter.TAG)
         if let handler=self.completionHandler{
             self.delegate=handler(self,nil)
         }
     }
     
     public func rewardVideoAdFailToLoad(_ ad: AlxRewardVideoAd, didFailWithError error: Error) {
-        NSLog("%@: rewardVideoAdLoad",AlgorixAdmobRewardAdapter.TAG)
+        NSLog("%@: rewardVideoAdLoad",AlgorixAdmobRewardVideoAdapter.TAG)
         if let handler=self.completionHandler{
             self.delegate=handler(nil,error)
         }
     }
     
     public func rewardVideoAdImpression(_ ad: AlxRewardVideoAd) {
-        NSLog("%@: rewardVideoAdImpression",AlgorixAdmobRewardAdapter.TAG)
+        NSLog("%@: rewardVideoAdImpression",AlgorixAdmobRewardVideoAdapter.TAG)
         self.delegate?.willPresentFullScreenView()
         self.delegate?.reportImpression()
     }
     
     public func rewardVideoAdClick(_ ad: AlxRewardVideoAd) {
-        NSLog("%@: rewardVideoAdClick",AlgorixAdmobRewardAdapter.TAG)
+        NSLog("%@: rewardVideoAdClick",AlgorixAdmobRewardVideoAdapter.TAG)
         self.delegate?.reportClick()
     }
     
     public func rewardVideoAdClose(_ ad: AlxRewardVideoAd) {
-        NSLog("%@: rewardVideoAdClose",AlgorixAdmobRewardAdapter.TAG)
+        NSLog("%@: rewardVideoAdClose",AlgorixAdmobRewardVideoAdapter.TAG)
         self.delegate?.didDismissFullScreenView()
     }
     
     public func rewardVideoAdPlayStart(_ ad:AlxRewardVideoAd) {
-        NSLog("%@: rewardVideoAdPlayStart",AlgorixAdmobRewardAdapter.TAG)
+        NSLog("%@: rewardVideoAdPlayStart",AlgorixAdmobRewardVideoAdapter.TAG)
         self.delegate?.didStartVideo()
     }
     
     public func rewardVideoAdPlayEnd(_ ad: AlxRewardVideoAd) {
-        NSLog("%@: rewardVideoAdPlayEnd",AlgorixAdmobRewardAdapter.TAG)
+        NSLog("%@: rewardVideoAdPlayEnd",AlgorixAdmobRewardVideoAdapter.TAG)
         self.delegate?.didEndVideo()
     }
     
     public func rewardVideoAdReward(_ ad: AlxRewardVideoAd) {
-        NSLog("%@: rewardVideoAdReward",AlgorixAdmobRewardAdapter.TAG)
+        NSLog("%@: rewardVideoAdReward",AlgorixAdmobRewardVideoAdapter.TAG)
         self.delegate?.didRewardUser()
     }
     
     public func rewardVideoAdPlayFail(_ ad: AlxRewardVideoAd, didFailWithError error: Error) {
-        NSLog("%@: rewardVideoAdPlayFail",AlgorixAdmobRewardAdapter.TAG)
+        NSLog("%@: rewardVideoAdPlayFail",AlgorixAdmobRewardVideoAdapter.TAG)
     }
     
 }
